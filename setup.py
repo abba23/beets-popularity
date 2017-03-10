1267c4d62f0f770371744ca54c8fdbc4cf8fa74b
abba23 <628208@gmail.com>
2017-03-10 16:02:28 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+from os import path
+
+directory = path.abspath(path.dirname(__file__))
+readme_path = path.join(directory, 'README.md')
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert(readme_path, 'rst')
+except (IOError, ImportError):
+    long_description = open(readme_path, encoding='utf-8').read()
+
+setup(
+    name = 'beets-popularity',
+    version = '1.0.0',
+    description = 'Beets plugin to store the popularity values from Spotify as flexible item attributes in the database',
+    long_description = long_description,
+    url = 'https://github.com/abba23/beets-popularity',
+    download_url = 'https://github.com/abba23/beets-popularity.git',
+    author = 'abba23',
+    author_email = '628208@gmail.com',
+    license = 'MIT',
+    platforms = 'ALL',
+    classifiers = [
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Multimedia :: Sound/Audio',
+        'Environment :: Console',
+    ],
+    packages = ['beetsplug'],
+    namespace_packages = ['beetsplug'],
+    install_requires = ['beets>=1.4.3', 'requests>=2.13.0'],
+)
